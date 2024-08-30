4d33582434888dc59abf087533573cad570c9b85
Dyniaaa <piotrkkardynal@gmail.com>
2024-08-30 12:48:48 +0200

    galeria

--- src/Components/Apartments/ApartmentsGallery/Gallery/Gallery.js
+++ src/Components/Apartments/ApartmentsGallery/Gallery/Gallery.js
@@ -7,26 +7,27 @@ const images1 = [
   {
     original: "/GalleryPhotos/APT1.1/1.Sypialnia1.jpg",
     thumbnail: "/GalleryPhotos/APT1.1/1.Sypialnia1.jpg",
-  },
-  {
-    original: "/GalleryPhotos/APT1.1/2.Sypialnia2.jpg",
-    thumbnail: "/GalleryPhotos/APT1.1/2.Sypialnia2.jpg",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT1.1/3.Salon1.jpg",
     thumbnail: "/GalleryPhotos/APT1.1/3.Salon1.jpg",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT1.1/4.Salon2.jpg",
     thumbnail: "/GalleryPhotos/APT1.1/4.Salon2.jpg",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT1.1/5.Łazienka.jpeg",
     thumbnail: "/GalleryPhotos/APT1.1/5.Łazienka.jpeg",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT1.1/6.Kuchnia1.jpg",
     thumbnail: "/GalleryPhotos/APT1.1/6.Kuchnia1.jpg",
+    thumbnailClass: "ThumbnailClass",
   },
 ];
 
@@ -34,38 +35,47 @@ const images2 = [
   {
     original: "/GalleryPhotos/APT2.1/1.Kuchnia1.jpg",
     thumbnail: "/GalleryPhotos/APT2.1/1.Kuchnia1.jpg",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT2.1/2.Kuchnia2.jpg",
     thumbnail: "/GalleryPhotos/APT2.1/2.Kuchnia2.jpg",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT2.1/3.Korytarz.jpg",
     thumbnail: "/GalleryPhotos/APT2.1/3.Korytarz.jpg",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT2.1/4.Salon1.jpg",
     thumbnail: "/GalleryPhotos/APT2.1/4.Salon1.jpg",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT2.1/5.Salon2.jpg",
     thumbnail: "/GalleryPhotos/APT2.1/5.Salon2.jpg",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT2.1/6.Łazienka1.JPEG",
     thumbnail: "/GalleryPhotos/APT2.1/6.Łazienka1.JPEG",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT2.1/7.Łazienka2.JPEG",
     thumbnail: "/GalleryPhotos/APT2.1/7.Łazienka2.JPEG",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT2.1/8.Sypialnia1.jpg",
     thumbnail: "/GalleryPhotos/APT2.1/8.Sypialnia1.jpg",
+    thumbnailClass: "ThumbnailClass",
   },
   {
     original: "/GalleryPhotos/APT2.1/9.Sypialnia2.jpg",
     thumbnail: "/GalleryPhotos/APT2.1/9.Sypialnia2.jpg",
+    thumbnailClass: "ThumbnailClass",
   },
 ];
 
@@ -75,10 +85,10 @@ class MyGallery extends React.Component {
       <div className="galleryBox">
         <h2>Apartamenty</h2>
         <div>
-          <h3>Apartament 1.1</h3>
           <ImageGallery
             items={images1}
             showPlayButton={false}
+            showFullscreenButton={false}
             additionalClass={"gallery1"}
           />
         </div>
@@ -87,6 +97,7 @@ class MyGallery extends React.Component {
           <ImageGallery
             items={images2}
             showPlayButton={false}
+            showFullscreenButton={false}
             additionalClass={"gallery2"}
           />
         </div>
