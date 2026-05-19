14d137e673929a458f5bcda247bdc6c02841b7ab
PiotrDK <piotrkkardynal@gmail.com>
2026-05-19 20:48:33 +0200

    version update3

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,37 @@
+import React from "react";
 import "@testing-library/jest-dom";
+
+class IntersectionObserver {
+  constructor() {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+Object.defineProperty(window, "IntersectionObserver", {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver,
+});
+
+Object.defineProperty(global, "IntersectionObserver", {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver,
+});
+
+const i18nMock = {
+  language: "pl",
+  changeLanguage: () => new Promise(() => {}),
+};
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    t: (key) => key,
+    i18n: i18nMock,
+  }),
+  withTranslation: () => (Component) => (props) => (
+    <Component t={(key) => key} i18n={i18nMock} {...props} />
+  ),
+  initReactI18next: { type: "3rdParty", init: () => {} },
+}));
