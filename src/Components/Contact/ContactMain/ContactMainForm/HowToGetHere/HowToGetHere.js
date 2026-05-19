dce92d416eea9cfe1c52bfd7c45c58bb78badb34
PiotrDK <piotrkkardynal@gmail.com>
2026-05-19 20:59:52 +0200

    Contact icons update

--- src/Components/Contact/ContactMain/ContactMainForm/HowToGetHere/HowToGetHere.js
+++ src/Components/Contact/ContactMain/ContactMainForm/HowToGetHere/HowToGetHere.js
@@ -13,12 +13,59 @@ class HowToGetHere extends Component {
           <p>28-100, Busko-Zdrój</p>
           <p>ul. Uchnasta 3</p>
           <div>
-            <img src={require("./PhoneIcon.jpg")} alt="phoneIcon" />
-            <a href="tel:504253973">504 253 973</a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-mail"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
+              <path d="M3 7l9 6l9 -6" />
+            </svg>
+            <p>apartamentyuchnasta3@gmail.com</p>
           </div>
           <div>
-            <img src={require("./PhoneIcon.jpg")} alt="phoneIcon" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-phone"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+            </svg>
             <a href="tel:884875800">884 875 800</a>
+          </div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-phone"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+            </svg>
+            <a href="tel:504253973">504 253 973</a>
           </div>
         </div>
         <div className="contactMapDiv">
